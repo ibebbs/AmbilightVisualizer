d4dc946a5cef4bd3113cc23458f9de371e9dad3b
ibebbs <github@bebbs.co.uk>
2015-02-23 13:01:35 +0000

    Added test fixture

---
+++ Ambilight.Test/DeserialisationTestFixture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RestSharp.Deserializers;
+
+namespace Ambilight.Test
+{
+    [TestClass]
+    public class DeserialisationTestFixture
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
